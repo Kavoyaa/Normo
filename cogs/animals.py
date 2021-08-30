30787161d8366c71df84e481a97fa66b20299788
Kavoya <78526936+Kavoyaa@users.noreply.github.com>
2021-08-31 00:07:39 +0530

    Commented every cog.

--- cogs/animals.py
+++ cogs/animals.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
+from main import p
 
 class Animals(commands.Cog):
+	global p
+	
 	def __init__(self, client):
 		self.client = client
 
+	# When the cog is loaded
 	@commands.Cog.listener()
 	async def on_ready(self):
 		print(f'[LOGS] {self.__class__.__name__} cog has been loaded.\n')
