fe4b47a90dba97627f2122b601c408a2baa5aece
Kavoya <78526936+Kavoyaa@users.noreply.github.com>
2021-10-02 17:40:40 +0530

    Revert "New command: 'wanted'."
    This reverts commit ee5e1e642331a3d34425208bda304681f787ef25.

--- cogs/images.py
+++ cogs/images.py
@@ -2,9 +2,6 @@
 import discord
 from discord.ext import commands
 from main import p
-from PIL import Image
-from io import BytesIO
-import os
 
 class Images(commands.Cog):
 	global p
@@ -16,28 +13,6 @@ class Images(commands.Cog):
 	@commands.Cog.listener()
 	async def on_ready(self):
 		print(f'[LOGS] {self.__class__.__name__} cog has been loaded.\n')
-
-	# Wanted command
-	@commands.command(name='wanted', aliases=['Wanted', 'WANTED'], description='Generates a wanted poster of the given user.')
-	async def wanted(self, ctx, user_: discord.Member=None):
-		user = user_
-		if user == None:
-			user = ctx.author
-
-		wanted = Image.open('images/wanted.jpg')
-		asset = user.avatar_url_as(size=128)
-		data = BytesIO(await asset.read())
-		pfp = Image.open(data)
-		pfp = pfp.resize((177, 177))
-		wanted.paste(pfp, (120, 212))
-		# Creates a 'wanted_output.jpg' which gets sent as output
-		wanted.save('wanted_output.jpg')
-
-		await ctx.send(file = discord.File('wanted_output.jpg'))
-		print(f'[LOGS] Command used: {p}wanted')
-
-		# Deletes the saved image file
-		os.remove('wanted_output.jpg')
 
 def setup(client):
 	client.add_cog(Images(client))
